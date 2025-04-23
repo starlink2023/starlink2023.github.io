965e1af0699828138cac9650e49080ac2377a74e
starlink2023 <139379125+starlink2023@users.noreply.github.com>
2025-04-23 13:49:19 +0200

    dist

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// filepath: c:\Users\sacco\Desktop\speriamo-main\vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: './', // Imposta il percorso base per GitHub Pages
+});
